a4c5139683b7127759821a84ff856575f3e7174b
kwakufosu <kwakuboachiefosu@gmail.com>
2023-02-27 12:55:51 +0000

    refactoredcode

--- src/services/email/email.ts
+++ src/services/email/email.ts
@@ -7,25 +7,42 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 const sen = seneca();
 
-sen.add({ area: "email", action: "send_email" }, function (args, respond) {
-  const msg = {
-    to: "kbfosu@st.ug.edu.gh", //will be dynamic
-    from: "kwakuboachiefosu@gmail.com",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js", //will be dynamic
-    html: `${args.message}`, //will be dynamic
-  };
+interface argsTemplate {
+  message: string;
+}
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      respond(null, { result: "Email sent" });
-    })
-    .catch((e) => {
-      respond(e, null);
-    });
-});
+let plugin = function (this: any, options: any) {
+  let sen = this;
+  sen.add(
+    { area: "email", action: "send_email" },
+    function (
+      args: argsTemplate,
+      respond: (
+        arg0: { e: Error } | null,
+        arg1: { result: string } | null
+      ) => void
+    ) {
+      const msg = {
+        to: "kbfosu@st.ug.edu.gh", //will be dynamic
+        from: "kwakuboachiefosu@gmail.com",
+        subject: "Sending with Twilio SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js", //will be dynamic
+        html: `${args.message}`, //will be dynamic
+      };
 
+      sgMail
+        .send(msg)
+        .then(() => {
+          respond(null, { result: "Email sent" });
+        })
+        .catch((e) => {
+          respond(e, null);
+        });
+    }
+  );
+};
+
+/*
 // Testing to see if it works.It will be moved from here later on
 sen.act(
   { area: "email", action: "send_email", message: "Football today" },
@@ -37,3 +54,6 @@ sen.act(
     console.log(result);
   }
 );
+*/
+
+module.exports = plugin;
