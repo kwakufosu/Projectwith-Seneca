c77e82d53e72aa9a7080d65acb8a098cc72a41d7
kwakufosu <kwakuboachiefosu@gmail.com>
2023-03-12 22:42:55 +0000

    Added the email field to the response object when an email is created

--- src/services-API.ts
+++ src/services-API.ts
@@ -9,21 +9,17 @@ export function api(this: any, options: any) {
 
   this.add("area:api,action: create", function (msg: any, respond: any) {
     let args = msg;
-    
 
     seneca.act(
       "area: note, action: create",
       args,
-      function (err: Error, result: any) {
-        if (err) {
-          respond(
-            Error(
-              "There's a problem with the email service. We will check and deliver it later"
-            ),
-            null
-          );
-        }
-        seneca.act("area: email, action:send_email", respond(null, result));
+      function (err: Error, note: any) {
+        seneca.act(
+          "area: email, action:send_email",
+          function (err: any, email: any) {
+            respond(err, { note, email });
+          }
+        );
       }
     );
   });
